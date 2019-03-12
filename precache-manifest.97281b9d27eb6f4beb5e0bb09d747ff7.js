4c7d3a1fa08ab9d7e32916898471b4d02c682abd
ubuntu <ubantu@yahoo.in>
2019-03-12 12:59:26 +0530

    init

---
+++ precache-manifest.97281b9d27eb6f4beb5e0bb09d747ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cca3cdeb3fde8a409cd",
+    "url": "/static/css/main.9555c5cb.chunk.css"
+  },
+  {
+    "revision": "4cca3cdeb3fde8a409cd",
+    "url": "/static/js/main.bdb5bcdc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cdbb0abdca2e439a1591",
+    "url": "/static/js/2.bc578357.chunk.js"
+  },
+  {
+    "revision": "a22195bedd0cbb31a696834cac43d03b",
+    "url": "/index.html"
+  }
+];
